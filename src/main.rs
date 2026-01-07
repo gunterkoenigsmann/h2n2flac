90284cfc29b2923c54f41c1105d8f6def06fe581
Gunter Königsmann <gunter@peterpall.de>
2026-01-07 23:01:14 +0100

    No support for 2-channel files yet. Totally untested, but compiles.

---
+++ src/main.rs
@@ -0,0 +1,218 @@
+extern crate getopts;
+extern crate sndfile;
+extern crate ndarray;
+use getopts::Options;
+use std::env;
+use std::fs;
+use sndfile::*;
+
+struct Recording {
+    name_xy: String,
+    name_ms: String,
+    outfilename: String,
+    xy_exists: bool,
+    ms_exists: bool,
+}
+
+impl Recording {
+    pub fn name_xy(&self) -> &String { &self.name_xy }
+    pub fn name_ms(&self) -> &String { &self.name_ms }
+    pub fn file_xy(&self) -> SndFile { return sndfile::OpenOptions::ReadOnly(ReadOptions::Auto).from_path(self.name_xy()).unwrap(); }
+    pub fn file_ms(&self) -> SndFile { return sndfile::OpenOptions::ReadOnly(ReadOptions::Auto).from_path(self.name_ms()).unwrap(); }
+    pub fn outfilename(&self) -> &String { &self.outfilename }
+    pub fn xy_exists(&self) -> &bool { &self.xy_exists }
+    pub fn ms_exists(&self) -> &bool { &self.ms_exists }
+    fn new(name: &str) -> Recording {
+        let filename_ms: String = name.replace("XY.WAV", "MS.WAV");
+        let filename_xy: String = name.replace("MS.WAV", "XY.WAV");
+        if filename_ms == filename_xy { panic!("No H2n filename found!");}
+
+        let xy_present: bool = match fs::exists(&filename_xy) {
+            Ok(x) => {x},
+            Err(e) => panic!("Cannot access the XY file: {e}")
+        };
+        let ms_present: bool = match fs::exists(&filename_ms) {
+            Ok(x) => {x},
+            Err(e) => panic!("Cannot access the MS file: {e}")
+        };
+
+        if !(xy_present || ms_present)
+        {
+            panic!("No audio files found for {}.", name);
+        }
+        let outfile: String = filename_ms.replace("MS.WAV", ".flac");
+        
+        Recording{
+            name_xy: filename_xy,
+            name_ms: filename_ms,
+            xy_exists: xy_present,
+            ms_exists: ms_present,
+            outfilename: outfile
+        }
+    }
+    
+    pub fn maxval(mut file: SndFile) -> f32 {
+        let mut maxval: f32 = 0.0;
+
+        let channels = file.get_channels();
+        if channels != 2
+        {
+            panic!("Channel count of the file is unexpected.");
+        }
+        let num_frames = 8192;
+        let mut buffer = vec![0.0f32; num_frames * channels];
+
+        loop {
+            match file.read_to_slice(&mut buffer) {
+                Ok(samples_read) => {
+                    for i in 0 .. samples_read
+                    {
+                        let absval = buffer[i].abs();
+                        if absval > maxval
+                        {
+                            maxval = absval;
+                        }
+                    }
+                    if samples_read < 8192
+                    {
+                        break;
+                    }
+                }
+                Err(e) => { panic!("Error reading file: {:?})", e); }
+            }
+        }
+        return maxval;
+    }
+}
+
+fn h2n2flac_4ch(normalize: bool, file: Recording) {
+
+    // Determine how strong to scale the audio data for normalizing the file
+    let mut scale_factor: f32 = 1.0;
+    if normalize
+    {
+        let maxval_xy: f32 = Recording::maxval(file.file_xy());
+        let maxval_ms: f32 = Recording::maxval(file.file_ms());
+        if maxval_xy > maxval_ms
+        {
+            scale_factor = 1.0/maxval_ms;
+        }
+        else
+        {
+            scale_factor = 1.0/maxval_xy;
+        }
+    }
+    
+    let mut msfile = file.file_ms();
+    let mut xyfile = file.file_xy();
+    if msfile.get_samplerate() != xyfile.get_samplerate()
+    {
+        panic!("MS and XY file differ in sample rate!");
+    }
+    if msfile.len() != xyfile.len()
+    {
+        panic!("MS and XY file differ in size!");
+    }
+    let mut outfile = sndfile::OpenOptions::WriteOnly(
+        WriteOptions::new(
+            MajorFormat::FLAC,
+            SubtypeFormat::PCM_24,
+            Endian::File,
+            msfile.get_samplerate(),
+            4
+        )
+    ).from_path(file.outfilename())
+        .expect("Failed to open file for writing");
+    
+    let num_frames = 8192;
+    let mut buffer_ms     = vec![0.0f32; num_frames * 2];
+    let mut buffer_xy     = vec![0.0f32; num_frames * 2];
+    let mut buffer_output = vec![0.0f32; num_frames * 4];
+
+    loop {
+        let n_samples: usize;
+        match msfile.read_to_slice(&mut buffer_ms) {
+            Ok(samples_read) => {
+                n_samples = samples_read;
+                for i in 0 .. samples_read / 2
+                {
+                    buffer_output[i*4+0] = buffer_ms[i*2+0] * scale_factor;
+                    buffer_output[i*4+1] = buffer_ms[i*2+1] * scale_factor;
+                }
+            }
+            Err(e) => { panic!("Error reading file: {:?})", e); }
+        }
+        match xyfile.read_to_slice(&mut buffer_xy) {
+            Ok(samples_read) => {
+                if n_samples != samples_read
+                {
+                    panic!("MS and XY files differ in length!");
+                }
+                for i in 0 .. samples_read / 2
+                {
+                    buffer_output[i*4+0] = buffer_xy[i*2+2] * scale_factor;
+                    buffer_output[i*4+1] = buffer_xy[i*2+3] * scale_factor;
+                }
+            }
+            Err(e) => { panic!("Error reading file: {:?})", e); }
+        }
+        outfile.write_from_slice(&mut buffer_output)
+            .expect("Failed to write audio data");
+        if n_samples < num_frames
+        {
+            break;
+        }
+    }
+}
+
+fn h2n2flac(normalize: bool, file: Recording) {
+
+    if *file.xy_exists() && *file.ms_exists()
+    {
+        h2n2flac_4ch(normalize, file);
+    }    
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let mut normalize: bool = false;
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help",      "print this help menu");
+    opts.optflag("v", "version",   "print the version info");
+    opts.optflag("n", "normalize", "Normalize the output");
+//    opts.optflagopt("c", "clip", "Allow clipping of n permille of the samples", "CLIP_PERCENTAGE");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()); }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    if matches.opt_present("n") {
+        normalize = true;
+    }
+    // if matches.opt_present("c") {
+    //     clip = match matches.opt_str("c") {
+    //         Some(s) => match s.parse::<f32>() {
+    //             Ok(n) => n,
+    //             Err(e) => panic!("The argument to c must be an integer. Got {e}"),
+    //         }
+    //         None => 1,
+    //     };
+    // }
+
+    // Rust doesn't like matches.free.for_each(move |file| h2n2flac(normalize, clip, file));
+    for file in &*matches.free
+    {
+        h2n2flac(normalize, Recording::new(file));
+    }
+
+}
